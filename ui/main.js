65120cf27b108c73fe812dade716e5cf67b3a895
Aashcharya <30959859+Aashcharya@users.noreply.github.com>
2017-08-15 22:47:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,9 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML ='New value';
-var img=document.getElementById('puffy');
+
+
+var img=document.getElementById('img');
 var marginleft =0;
 function moveRight()
 {
